8a7ceac1dadd6d39a80cc099608e2c34c8c0650d
Andrew Wand <andrewwands@gmail.com>
2022-08-01 18:19:53 -0500

    set up home page

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello world!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Mini Messageboard', messages: messages });
+});
+
+
+
+
+
+
+
+// router.post('/new', function(req, res, next) {
+//   console.log(req.body.messageText);
+//   console.log(req.body.authorName);
+// })
+
+module.exports = router;
